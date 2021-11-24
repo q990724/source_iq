50f38e2904e572b52813e7200f6fc744034330c6
q990724 <664625200@qq.com>
2021-11-24 22:10:06 +0800

    Update text-search.vue

--- src/views/Search/components/text-search.vue
+++ src/views/Search/components/text-search.vue
@@ -153,7 +153,7 @@ export default {
         beforeAvatarUpload(file) {
             const isLt2M = file.size / 1024 / 1024 < 2;
             if (!isLt2M) {
-                this.$message.error('上传头像图片大小不能超过 2MB!');
+                this.$message.error('上传图片大小不能超过 2MB!');
             }
             return isLt2M;
         },
