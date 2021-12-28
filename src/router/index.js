6a2de6bf2799d7e1a0792e645750e13a0bc5bb04
q990724 <664625200@qq.com>
2021-12-28 09:38:23 +0800

    动态生成routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import SourceMap from "@/assets/js/source_map";
 const routerPush = VueRouter.prototype.push;
 
 VueRouter.prototype.push = function push(location) {
@@ -9,98 +9,122 @@ VueRouter.prototype.push = function push(location) {
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: '/',
-        name: 'Index',
-        redirect: '/layout/view-alibaba'
-    },
-    {
-        path: '/layout',
-        name: 'Layout',
-        component: () => import('@/layout/layout'),
-        children: [
-            {
-                path: 'view-alibaba',
-                name: 'view-alibaba',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-aliexpressDS',
-                name: 'view-aliexpressDS',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-aliexpressZapieX',
-                name: 'view-aliexpressZapieX',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-1688',
-                name: 'view-1688',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-1688rapid',
-                name: 'view-1688rapid',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-1688global',
-                name: 'view-1688global',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-yiwugo',
-                name: 'view-yiwugo',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-dhgate',
-                name: 'view-dhgate',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-mic',
-                name: 'view-mic',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-cjds',
-                name: 'view-cjds',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-litbox',
-                name: 'view-litbox',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-1688overseas',
-                name: 'view-1688overseas',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-banggood',
-                name: 'view-banggood',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-chinabrands',
-                name: 'view-chinabrands',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-            {
-                path: 'view-globalres',
-                name: 'view-globalres',
-                component: () => import('@/views/Search/search-views/search_result')
-            },
-        ]
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'Index',
+//         redirect: '/layout/view-alibaba'
+//     },
+//     {
+//         path: '/layout',
+//         name: 'Layout',
+//         component: () => import('@/layout/layout'),
+//         children: [
+//             {
+//                 path: 'view-alibaba',
+//                 name: 'view-alibaba',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-aliexpressDS',
+//                 name: 'view-aliexpressDS',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-aliexpressZapieX',
+//                 name: 'view-aliexpressZapieX',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-1688',
+//                 name: 'view-1688',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-1688rapid',
+//                 name: 'view-1688rapid',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-1688global',
+//                 name: 'view-1688global',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-yiwugo',
+//                 name: 'view-yiwugo',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-dhgate',
+//                 name: 'view-dhgate',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-mic',
+//                 name: 'view-mic',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-cjds',
+//                 name: 'view-cjds',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-litbox',
+//                 name: 'view-litbox',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-1688overseas',
+//                 name: 'view-1688overseas',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-banggood',
+//                 name: 'view-banggood',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-chinabrands',
+//                 name: 'view-chinabrands',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//             {
+//                 path: 'view-globalres',
+//                 name: 'view-globalres',
+//                 component: () => import('@/views/Search/search-views/search_result')
+//             },
+//         ]
+//     }
+// ]
+
+let routes = [];
+let layout = {
+    path: '/layout',
+    name: 'Layout',
+    component: () => import('@/layout/layout'),
+    children: []
+}
+SourceMap.forEach(e=>{
+    let route = {
+        path: `view-${e.petName}`,
+        name: `${e.name}`,
+        component: () => import('@/views/Search/search-views/search_result')
     }
-]
+    layout.children.push(route);
+})
+routes[0] = {
+    path: '/',
+    name: 'Index',
+    redirect: `/layout/${layout.children[0].path}`
+}
+routes[1] = layout;
 
 const router = new VueRouter({
-    routes
+    routes: routes
 })
+
+console.log(router);
 
 export default router
